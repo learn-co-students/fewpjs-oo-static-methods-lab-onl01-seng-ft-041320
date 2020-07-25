e71f415d25d6e9c37af3ff8e75d30b73da9f50ea
ReginaF2012 <skritschy@gmail.com>
2020-07-25 00:20:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+
+  static titleize(string){
+    let dontCapitalize = ['a', 'an', 'but', 'of', 'for', 'at', 'by', 'from', 'the', 'and']
+    string = Formatter.capitalize(string)
+    let splitString = string.split(' ')
+    splitString = splitString.map(word => {
+      if (!dontCapitalize.includes(word)){
+        word = Formatter.capitalize(word)
+      }
+      return word
+    })
+    return splitString.join(' ')
+  }
 }
