132f85eb2a69719470a6ef6b0e7ece953ceb7512
mattenbar <matthw.enbar@gmail.com>
2020-07-27 17:37:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(string){
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'-' ']+/g, '');
+  }
+
+  static titleize (string){
+    let dontCapitalize = ['a', 'an', 'but', 'of', 'for', 'at', 'by', 'from', 'the', 'and']
+    
+    string = this.capitalize(string)
+    let stringArray = string.split(' ')
+    stringArray = stringArray.map(word =>{
+      if(!dontCapitalize.includes(word)){
+        word = this.capitalize(word)
+      }
+      return word
+    })
+    return stringArray.join(' ')
+  }
+
 }
