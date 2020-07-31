824c6b0e5d14965768c8f3f11459b0632016e09f
dbhmunro <dbhmunro@gmail.com>
2020-07-31 14:24:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+
+  static titleize(string) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let arr = string.split(" ")
+    arr[0] = Formatter.capitalize(arr[0])
+    return arr.map( function(e) {
+      if ( !exceptions.some( word => word === e ) ) {
+        return Formatter.capitalize(e)
+      } else {
+        return e
+      }
+    }).join(" ")
+  }
 }
