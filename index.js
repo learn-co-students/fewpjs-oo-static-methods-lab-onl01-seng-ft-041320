fa3515e7aabec6373d2772439a945ec8da76242c
markcascardi <mcascardi@gmail.com>
2020-08-01 21:15:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string){
+    let exclusions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    string = Formatter.capitalize(string)
+    let splitString = string.split(" ")
+    const newArr = []
+    splitString.map(word => {
+      if (!exclusions.includes(word)) {
+        word = Formatter.capitalize(word)
+      }
+      newArr.push(word)
+      return newArr
+    })
+    return newArr.join(" ")
+  }
 }
