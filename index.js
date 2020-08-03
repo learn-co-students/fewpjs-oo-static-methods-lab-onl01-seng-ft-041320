5ac8e03646f6e565793b5159c136a10957028308
Dhrubot <dhrubo.dh@gmail.com>
2020-08-02 22:39:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-'-' ']+/g, '');
+  }
+
+  static titleize(string) {
+    let nonCapitalize = ['a', 'an', 'but', 'of', 'for', 'at', 'by', 'from', 'the', 'and']
+    string = this.capitalize(string)
+    let stringArray = string.split(' ')
+    stringArray = stringArray.map(word => {
+      if (!nonCapitalize.includes(word)) {
+        word = this.capitalize(word)
+      }
+      return word
+    })
+    return stringArray.join(' ')
+  }
 }
